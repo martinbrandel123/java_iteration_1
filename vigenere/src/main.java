012e6b94dd2099c930f99892433f642bf45a0649
Martin BRANDEL <martin.brandel@Digital-Grenoble.local>
2022-05-11 11:36:59 +0200

    commit

--- vigenere/src/main.java
+++ vigenere/src/main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class main {
+import java.util.Arrays;
+
+
+public class main {
+    public static void main(String args[]){
+//        String[] wordToCrypt;
+//        wordToCrypt= new String[]{"c","a","m","p","u","s"};
+//        String dico = "abcdefghijklmnopqrs";
+//       // System.out.println(dico.indexOf("c"));
+
+
+        String alphabet = "abcdefghijk";
+        String mdp = "dgbj";
+        String mot = "campuscam";
+
+        for (int i = 0; i < mot.length(); i++) {
+            char matching_letter = mdp.charAt(i%mdp.length());
+            int decalage = alphabet.indexOf(matching_letter);
+            char lettre_a_encoder =  mot.charAt(i);
+            int index_lettre_a_encoder = alphabet.indexOf(lettre_a_encoder);
+
+            System.out.println("i : " + i);
+            System.out.println("lettre à encoder : " + lettre_a_encoder);
+            System.out.println("i%3 : " + i%mdp.length());
+            System.out.println("matching letter in mdp : " + matching_letter);
+            System.out.println("decalge à appliquer : " + decalage);
+            System.out.println("--------------------");
+        }
+
+
+    }
 }
--- vigenere/src/vigenereversiondemerde.java
+++ vigenere/src/vigenereversiondemerde.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class vigenereversiondemerde {
+public class vigenereversiondemerde {
+
+    public void test (){
+        String dico = "abcdefhijkl";
+        System.out.println(dico.indexOf("c"));
+        System.out.println("--------------------------------");
+
+        String[] wordToCrypt;
+        String[] wordToEncrypt = new String[6];
+        String[] alphabet;
+        int[] key;
+        String newWord = "";
+
+        alphabet=new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        wordToCrypt=new String[]{"c","a","m","p","u","s"};
+        String mdp = "abcabc";
+        key = new int[]{0,1,2,0,1,2};
+
+        for (int i = 0; i < wordToCrypt.length; i++){
+            for (int j = 0; j < alphabet.length; j++){
+                if(wordToCrypt[i].equals(alphabet[j])){
+                    wordToEncrypt[i] = alphabet[j + key[i]];                }
+            }
+        }
+        for (int i = 0; i < wordToCrypt.length; i++){
+            for (int j = 0; j < alphabet.length; j++){
+                if(wordToEncrypt[i].equals(alphabet[j])){
+                    newWord += alphabet[j - key[i]];
+                }
+            }
+        }
+        System.out.println(newWord);
+    }
+
 }
